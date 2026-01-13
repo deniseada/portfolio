79d5e2907541ebc032ac08660c3109918395c72a
Denise Aquino <daquino10@my.bcit.ca>
2026-01-13 00:05:53 -0800

    add framework next js

---
+++ app/page.js
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <div>
+          <h1 className={styles.title}>Coming Soon!</h1>
+        </div>
+      </main>
+    </div>
+  );
+}
